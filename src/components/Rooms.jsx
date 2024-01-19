4302564358c6f0b512ce5d5955a4c6fcfbc73d0d
SanskarSinghRajput <you@example.com>
2024-01-19 17:15:24 +0530

    first commit

---
+++ src/components/Rooms.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Rooms = () => {
+  return (
+    <div className='maw-w-[1400px] h-[500px] bg-blue-100 mx-auto my-20 '>
+      <div>
+        <h3>Fine Interior Rooms</h3>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi repellendus, sint at modi cumque reprehenderit?</p>
+      </div>
+
+      <div>
+      <img
+          src='https://images.unsplash.com/photo-1602002418082-a4443e081dd1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80'
+          alt='/'
+        />
+        <img
+          src='https://images.unsplash.com/photo-1512918728675-ed5a9ecdebfd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80'
+          alt='/'
+        />
+        <img
+          src='https://images.unsplash.com/photo-1582719478250-c89cae4dc85b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80'
+          alt='/'
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Rooms
